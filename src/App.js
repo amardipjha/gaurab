93bf227c868f708b2bbfb8b6622c190a0345211c
Amardip Jha <amardip062@gmail.com>
2020-12-29 12:13:09 +0530

    First react Website

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <h1>Hello Customer.</h1>
+      <h2>It is my First react app to upload the Github Pages.</h2>
+      <h3>Thank You !</h3>
+      </>
+    </div>
+  );
+}
+
+export default App;
